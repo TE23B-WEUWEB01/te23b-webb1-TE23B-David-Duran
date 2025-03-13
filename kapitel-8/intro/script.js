7116c2c3bad1ca6a8dda8998c0b38d5f9f318d23
David Duran <david.duranherrera@elev.ga.ntig.se>
2025-03-13 11:07:27 +0100

    Vi js, variabler och konkatenering (sätta ihop text)

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,15 @@
+// var ålder = 17;
+// var tal1 = 24;
+// var tal2 = 3;
+// var summa = tal1 + tal2;
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+var förnamn = "David";
+var efternamn = "Duran";
+var helaNamnet = förnamn + " " + efternamn; 
+var hobby = "basket";
+var kostnad = "1500"
+
+
+var mening = helaNamnet + "'s hobby är " + hobby + ".\nMin hobby kostar" + kostnad + "kr/år.";
+alert(mening); 
